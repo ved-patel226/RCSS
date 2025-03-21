386fe3986b31150eca75d985d623f880385be91e
Ved Patel <talk2ved11@gmail.com>
2025-03-21 16:56:30 -0400

    Enhance parser by adding string literal support in del_value and refactor test structure

--- src/tests/test.rs
+++ src/tests/test.rs
@@ -1,18 +1,4 @@
-mod rcss {
-    pub mod compiler;
-    pub mod errors;
-
-    pub mod process_x {
-        pub mod function;
-        pub mod media_query;
-        pub mod variable;
-        pub mod keyframes;
-    }
-}
-
-mod tests {
-    pub mod test;
-}
+use crate::{ rcss, RCSSParser, Rule };
 
 use notify::event::{ AccessKind, AccessMode };
 use notify::{ recommended_watcher, Event, RecursiveMode, Result, Watcher, EventKind };
@@ -42,89 +28,13 @@ use rcss::{
     compiler::{ process_rule, MetaDataValue },
 };
 
-#[derive(Parser)]
-#[grammar = "rcss.pest"]
-pub struct RCSSParser;
+#[test]
+fn website_tests() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let input_path = "src/tests/rcss/test.ccss";
+    let output_path = "src/tests/css";
+    let verbose = true;
+    let human_readable = true;
 
-fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let matches = Command::new("RCSS Compiler")
-        .version("1.0.0")
-        .about("Compiles RCCS files to CSS")
-        .arg(Arg::new("input").help("Input directory to process").required(true).index(1))
-        .arg(
-            Arg::new("minify")
-                .short('m')
-                .long("minify")
-                .help("Minify the output CSS")
-                .action(clap::ArgAction::SetTrue)
-        )
-        .arg(
-            Arg::new("verbose")
-                .short('v')
-                .long("verbose")
-                .help("Print verbose processing information")
-                .action(clap::ArgAction::SetTrue)
-        )
-        .get_matches();
-
-    // Get input and output file paths
-    let input_path = matches.get_one::<String>("input").unwrap();
-
-    let verbose = matches.get_flag("verbose");
-    let human_readable = !matches.get_flag("minify");
-
-    if verbose {
-        println!("Reading from {}", input_path);
-    }
-
-    let (tx, rx) = mpsc::channel::<Result<Event>>();
-
-    let mut watcher = recommended_watcher(tx)?;
-
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    watcher.watch(Path::new(input_path), RecursiveMode::Recursive)?;
-
-    for res in rx {
-        match res {
-            Ok(path) => {
-                // if file is written to
-                if let EventKind::Access(AccessKind::Close(AccessMode::Write)) = path.kind {
-                    if path.paths[0].extension().and_then(|s| s.to_str()) == Some("rcss") {
-                        let filename_stem = path.paths[0]
-                            .file_stem()
-                            .and_then(|stem| stem.to_str())
-                            .unwrap_or("default");
-
-                        let relative_css_path = path.paths[0]
-                            .parent()
-                            .unwrap_or(Path::new("."))
-                            .join("../css")
-                            .join(filename_stem);
-
-                        let _ = compile(
-                            &path.paths[0].display().to_string(),
-                            &(relative_css_path.to_str().unwrap().to_string() + ".css"),
-                            verbose,
-                            human_readable
-                        );
-                    }
-                }
-            }
-
-            Err(e) => println!("watch error: {:?}", e),
-        }
-    }
-
-    Ok(())
-}
-
-fn compile(
-    input_path: &str,
-    output_path: &str,
-    verbose: bool,
-    human_readable: bool
-) -> std::result::Result<(), Box<dyn std::error::Error>> {
     let start_time = Instant::now();
 
     let unparsed_css = fs::read_to_string(input_path).map_err(|e| {
